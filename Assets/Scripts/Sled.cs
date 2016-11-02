03a53f7308b26d844d918a928ff367adcc3eef9d
Ashley Ervin <aee9149@rit.edu>
2016-11-02 09:06:30 -0400

    Movement and basic input for controllers work. A to push gas, tap stick left/right to turn. Car drifts, will be fixing that and making purposeful drifting next.

---
+++ Assets/Scripts/Sled.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sled : MonoBehaviour {
+	float x;
+
+	float deadzone = 0.2f;
+	float maxSpeed = 2f;
+	float speed = 0.0f;
+	float acceleration = 0.005f;
+	float drag = 0.002f;
+
+	Vector2 dir;
+
+	public GameObject gSled;
+	public Sled(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.GetAxis ("Horizontal");
+
+		if(Input.GetButton("A")) {
+			//'Debug.Log("A");
+			if(x !=0 ){
+				Turn();
+			}
+			Move ();
+		}
+	}
+
+	public void Turn(){
+		Debug.Log ("TUrn");
+		Vector2 tmp = new Vector2 (x, 0);
+		Vector2 currentUp = transform.up;
+
+		Debug.Log (currentUp);
+
+		if (tmp.sqrMagnitude > deadzone) {
+			dir = tmp.normalized;
+		}
+		gSled.GetComponent<Rigidbody2D> ().AddTorque (-1 * x);
+		//transform.Rotate(new Vector3(0, 0,  * Time.deltaTime * 60));
+
+	}
+
+	public void Move(){
+		Debug.Log ("Move");
+		speed += acceleration * Time.deltaTime;
+		if (gSled.GetComponent<Rigidbody2D> ().velocity.magnitude > maxSpeed) {
+
+			gSled.GetComponent<Rigidbody2D> ().velocity.Normalize ();
+			gSled.GetComponent<Rigidbody2D> ().velocity *= maxSpeed;
+		}
+		gSled.GetComponent<Rigidbody2D> ().AddForce (transform.up);
+	}
+
+	public void Drift(){
+
+	}
+
+}
+
